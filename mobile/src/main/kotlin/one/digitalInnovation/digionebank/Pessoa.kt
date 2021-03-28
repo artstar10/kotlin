380dd393add161d2a9a188dd1d3f27a72cb32820
artstar10 <arthurnevesx@hotmail.com>
2021-03-27 22:48:20 -0300

    update  to talk about members

--- mobile/src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
+++ mobile/src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
@@ -2,15 +2,15 @@ package one.digitalInnovation.digionebank
 
 class Pessoa {
     var nome: String  = "Arthur"
-
     var cpf: String = "113.333.444-99"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
    val arthur = Pessoa()
-
-    println(arthur)
-    println(arthur.nome)
-    println(arthur.cpf)
+   println(arthur.pessoaInfo())
 }
