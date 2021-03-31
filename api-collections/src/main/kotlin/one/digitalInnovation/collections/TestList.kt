ac0bf7f9411217c3faef61de2bb79e02dc9b7dd2
artstar10 <arthurnevesx@hotmail.com>
2021-03-31 18:58:54 -0300

    Organizando grupos com groupBy

--- api-collections/src/main/kotlin/one/digitalInnovation/collections/TestList.kt
+++ api-collections/src/main/kotlin/one/digitalInnovation/collections/TestList.kt
@@ -2,9 +2,9 @@ package one.digitalInnovation.collections
 
 fun main() {
     /*criado 3 funcionários */
-    val joao = Funcionario("João", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0,  "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
     /* funcionario guardados na variavel funcionarios*/
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -18,11 +18,16 @@ fun main() {
         .sortedBy { it.salario }
         .forEach { println(it) }
 
+    println("==groupBy - ordenado por tipo de contratacao==")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String,
 ) {
     override fun toString(): String =
         """
