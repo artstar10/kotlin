38dd8bc55fceca65ff48a06ae018b0600de4fc77
artstar10 <arthurnevesx@hotmail.com>
2021-03-31 18:46:45 -0300

    Organizando a lista com sortedBy

--- api-collections/src/main/kotlin/one/digitalInnovation/collections/TestList.kt
+++ api-collections/src/main/kotlin/one/digitalInnovation/collections/TestList.kt
@@ -2,8 +2,8 @@ package one.digitalInnovation.collections
 
 fun main() {
     /*criado 3 funcionários */
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
     /* funcionario guardados na variavel funcionarios*/
     val funcionarios = listOf(joao, pedro, maria)
@@ -12,6 +12,12 @@ fun main() {
 
     println("==retorno do nome da maria==")
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("==sortedBy - ordenado em ordem crescente de salario==")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
 }
 
 data class Funcionario(
