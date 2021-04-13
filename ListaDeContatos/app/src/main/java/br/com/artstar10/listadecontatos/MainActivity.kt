112a38b422eb92d858fe6fde9fa3e2e44185d509
artstar10 <arthurnevesx@hotmail.com>
2021-04-12 21:04:47 -0300

    add: app inicial de lista de contatos

---
+++ ListaDeContatos/app/src/main/java/br/com/artstar10/listadecontatos/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.artstar10.listadecontatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
