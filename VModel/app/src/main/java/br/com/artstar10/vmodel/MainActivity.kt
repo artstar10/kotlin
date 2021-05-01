365f0facb6b7858b99aff6bbf5b93873d5f1f261
artstar10 <arthurnevesx@hotmail.com>
2021-05-01 18:46:20 -0300

    Ciclo de vida de uma activity - Pratica

--- VModel/app/src/main/java/br/com/artstar10/vmodel/MainActivity.kt
+++ VModel/app/src/main/java/br/com/artstar10/vmodel/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.artstar10.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -18,11 +19,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logar(valor = "Oncreate")
+
         initDados()
         initContador()
         initClick()
 
         validaContador()
+
+
+    }
+
+    override fun onStart(){
+        logar(valor = "onStart")
+        super.onStart()
+    }
+
+    override fun onResume(){
+        logar(valor = "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        logar(valor = "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logar(valor = "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logar(valor = "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun logar(tag: String = "Ciclo de Vida", valor: String){
+        Log.d(tag, valor)
     }
 
     private fun validaContador() {
@@ -39,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnMostrar.setOnClickListener{
-            Toast.makeText(this, "Valor Contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
+          Toast.makeText(this, "Valor Contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
         }
     }
 
