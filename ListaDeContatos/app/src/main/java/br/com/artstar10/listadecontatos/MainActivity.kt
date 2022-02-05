cb777510a5fa201126ef0eb460f82a6c64657221
Artstar10 <arthurnevesx@hotmail.com>
2022-02-05 19:07:58 -0300

    Desenvolvendo a classe MainActivity

--- ListaDeContatos/app/src/main/java/br/com/artstar10/listadecontatos/MainActivity.kt
+++ ListaDeContatos/app/src/main/java/br/com/artstar10/listadecontatos/MainActivity.kt
@@ -1,11 +1,35 @@
 package br.com.artstar10.listadecontatos
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    val READ_CONTACTS = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permissions.READ_CONTACTS)
+        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(tthis,
+            arrayOf(Manifest.permissions.READ_CONTACTS),
+            READ_CONTACTS)
+        } else {
+            setContacts()
+        }
     }
+
+    onRequest fun onRequestPermissionsResult(requestCode: Int,
+                                             permission: Array<out String>,
+                                             grantResult: IntArray) {
+    if (requestCode ==  READ_CONTACTS) setContacts()
+    }
+
+private fun setContacts() {
+// todo: no yet implements
 }
