84e3be912f4019d4b2c5750ce8f9b3e4d2be346e
Artstar10 <arthurnevesx@hotmail.com>
2022-02-28 16:47:42 -0300

    adicionando - Time Picker

---
+++ Todolist/app/src/main/java/br/com/artstar/todolist/datasource/TaskDataSource.kt
@@ -0,0 +1,13 @@
+package br.com.artstar.todolist.datasource
+
+import br.com.artstar.todolist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list
+
+    fun insertTask(task: Task) {
+        list.add(task.copy(id = list.size + 1))
+    }
+}
