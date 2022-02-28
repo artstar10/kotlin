0327924d08780cf5afd32e44bd56175cb2332ef5
Artstar10 <arthurnevesx@hotmail.com>
2022-02-28 15:50:40 -0300

    adicionando - ViewBinding e date picker

---
+++ Todolist/app/src/main/java/br/com/artstar/todolist/ui/AddTaskActivity.kt
@@ -0,0 +1,36 @@
+package br.com.artstar.todolist.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.artstar.todolist.databinding.ActivityAddTaskBinding
+import br.com.artstar.todolist.extensions.format
+import br.com.artstar.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG" )
+        }
+    }
+
+}
